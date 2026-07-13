7989381998de459a66cb989db436851e45564194
Yun Dou <douyun@dtyq.com>
2026-07-13 12:01:56 +0800

    feat(cli): add GetGitHTTPCredential for go-git

--- cli/util/git.go
+++ cli/util/git.go
@@ -1,14 +1,22 @@
 package util
 
 import (
+	"bufio"
+	"bytes"
+	"context"
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
 
+	"github.com/go-git/go-billy/v6/osfs"
 	"github.com/go-git/go-git/v6"
 	"github.com/go-git/go-git/v6/plumbing"
+	gitClient "github.com/go-git/go-git/v6/plumbing/client"
+	gitHttp "github.com/go-git/go-git/v6/plumbing/transport/http"
+	"github.com/go-git/go-git/v6/storage/filesystem"
 )
 
 func GitURLLocalPath(u string) string {
@@ -35,13 +43,13 @@ func GitURLLocalPath(u string) string {
 
 func InitEmptyGitRepo(dir string, bare bool) error {
 	isGitDir := func(dir string) bool {
-		if stat, err := os.Stat(path.Join(dir, "HEAD")); err != nil || stat.IsDir() {
+		if stat, err := os.Stat(filepath.Join(dir, "HEAD")); err != nil || stat.IsDir() {
 			return false
 		}
-		if stat, err := os.Stat(path.Join(dir, "objects")); err != nil || !stat.IsDir() {
+		if stat, err := os.Stat(filepath.Join(dir, "objects")); err != nil || !stat.IsDir() {
 			return false
 		}
-		if stat, err := os.Stat(path.Join(dir, "refs")); err != nil || !stat.IsDir() {
+		if stat, err := os.Stat(filepath.Join(dir, "refs")); err != nil || !stat.IsDir() {
 			return false
 		}
 		return true
@@ -51,7 +59,7 @@ func InitEmptyGitRepo(dir string, bare bool) error {
 		// bare repo
 		return fmt.Errorf("already exist")
 	}
-	if isGitDir(path.Join(dir, ".git")) {
+	if isGitDir(filepath.Join(dir, ".git")) {
 		// worktree repo
 		return fmt.Errorf("already exist")
 	}
@@ -63,4 +71,94 @@ func InitEmptyGitRepo(dir string, bare bool) error {
 	repo.Close()
 
 	return nil
+}
+
+func GetGoGitRepoGitDir(repo *git.Repository) string {
+	if repo == nil {
+		// ??
+		return ""
+	}
+	if fsStorage, ok := repo.Storer.(*filesystem.Storage); ok {
+		billyFs := fsStorage.Filesystem()
+		_, isBoundOS := billyFs.(*osfs.BoundOS)
+		_, isRootOS := billyFs.(*osfs.RootOS)
+		if isBoundOS || isRootOS {
+			// is osfs
+			return filepath.Clean(billyFs.Root())
+		}
+	}
+	return ""
+}
+
+func GetGitHTTPCredential(
+	ctx context.Context,
+	repo *git.Repository,
+	proto string,
+	host string,
+) (gitClient.HTTPAuth, error) {
+	if proto != "http" && proto != "https" {
+		return nil, fmt.Errorf("not implemented protocol: %s", proto)
+	}
+
+	// prepare environments
+	env := map[string]string{}
+	for _, environ := range os.Environ() {
+		pair := strings.SplitN(environ, "=", 2)
+		env[pair[0]] = pair[1]
+	}
+	// GIT_DIR
+	env["GIT_DIR"] = GetGoGitRepoGitDir(repo)
+	// GIT_WORK_DIR
+	// TODO
+
+	// execute helper
+	input := bytes.NewBufferString(fmt.Sprintf("protocol=%s\nhost=%s\n", proto, host))
+	output := bytes.NewBuffer(nil)
+	cmd := Command{
+		Args: []string{
+			"git",
+			"credential",
+			"fill",
+		},
+		Env:    env,
+		Stdin:  input,
+		Stdout: output,
+		Stderr: os.Stderr,
+	}
+	timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	err := cmd.Run(timeoutCtx)
+	if err != nil {
+		return nil, fmt.Errorf("failed exec cred helper: %w", err)
+	}
+
+	// parse output
+	scanner := bufio.NewScanner(output)
+	var username, password string
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		val := strings.TrimSpace(parts[1])
+
+		switch key {
+		case "username":
+			username = val
+		case "password":
+			password = val
+		}
+	}
+	if password == "" {
+		return nil, fmt.Errorf("credential helper gives no password")
+	}
+
+	cred := &gitHttp.BasicAuth{
+		Username: username,
+		Password: password,
+	}
+	return cred, nil
 }
--- cli/util/git_test.go
+++ cli/util/git_test.go
@@ -1,9 +1,15 @@
 package util
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/go-git/go-git/v6"
+	gitHttp "github.com/go-git/go-git/v6/plumbing/transport/http"
+	"github.com/go-git/go-git/v6/storage/memory"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -88,4 +94,135 @@ func TestGitURLLocalPath(t *testing.T) {
 			assert.Equal(t, tc.path, got, "GitURLLocalPath(%s) is no expected", tc.url)
 		})
 	}
+}
+
+func TestGetGoGitRepoGitDir(t *testing.T) {
+	t.Run("NotOSRepo", func(t *testing.T) {
+		storer := memory.NewStorage()
+		// worktreeFs := memfs.New()
+		repo, err := git.Init(storer, git.WithDefaultBranch("refs/heads/latest"))
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+		defer repo.Close()
+
+		assert.Empty(t, GetGoGitRepoGitDir(repo))
+	})
+	t.Run("Worktree", func(t *testing.T) {
+		tmpdir := t.TempDir()
+		repo, err := git.PlainInit(tmpdir, false, git.WithDefaultBranch("refs/heads/latest"))
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+
+		assert.NotEmpty(t, GetGoGitRepoGitDir(repo))
+		assert.DirExists(t, GetGoGitRepoGitDir(repo))
+		assert.DirExists(t, fmt.Sprintf("%s/objects", GetGoGitRepoGitDir(repo)))
+	})
+	t.Run("Bare", func(t *testing.T) {
+		tmpdir := t.TempDir()
+		repo, err := git.PlainInit(tmpdir, true, git.WithDefaultBranch("refs/heads/latest"))
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+
+		assert.NotEmpty(t, GetGoGitRepoGitDir(repo))
+		assert.DirExists(t, GetGoGitRepoGitDir(repo))
+		assert.DirExists(t, fmt.Sprintf("%s/objects", GetGoGitRepoGitDir(repo)))
+	})
+}
+
+func TestGetGitCredential(t *testing.T) {
+	cmd := Command{
+		Args: []string{
+			"git", "version",
+		},
+	}
+	err := cmd.Run(context.Background())
+	if err != nil {
+		t.Skip("skip no git")
+	}
+	t.Run("BadProto", func(t *testing.T) {
+		ctx := context.Background()
+		_, err := GetGitHTTPCredential(ctx, nil, "bad", "example.org")
+		assert.ErrorContains(t, err, "not implemented protocol")
+	})
+	t.Run("Common", func(t *testing.T) {
+		ctx := context.Background()
+		tmpdir := t.TempDir()
+
+		// generate fake helper for testing
+		err := os.WriteFile(filepath.Join(tmpdir, "fakehelper"), []byte(`#!/bin/sh
+# assert GIT_DIR
+if [ ! -d "${GIT_DIR}/objects" ]
+then
+	echo "GIT_DIR is not set" >&2
+	exit 1
+fi
+
+# read kv
+while read -r line
+do
+	k="${line%%=*}"
+	v="${line##*=}"
+	if [ "$k" = "protocol" ]
+	then
+		proto="$v"
+	elif [ "$k" = "host" ]
+	then
+		host="$v"
+	fi
+done
+
+if [ "$proto" != "https" ]
+then
+	echo bad proto "$proto" >&2
+	exit 1
+fi
+if [ "$host" != "example.org" ]
+then
+	echo bad host "$host" >&2
+	exit 1
+fi
+
+echo "protocol=$proto"
+echo "host=$host"
+echo "username=user" # gitleaks:allow
+echo "password=topsecret" # gitleaks:allow
+`), 0o755)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+
+		// generate git dir
+		repo, err := git.PlainInit(filepath.Join(tmpdir, "testrepo"), false, git.WithDefaultBranch("refs/heads/latest"))
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+		defer repo.Close()
+
+		// setup helper
+		f, err := os.OpenFile(filepath.Join(tmpdir, "testrepo", ".git", "config"), os.O_WRONLY|os.O_APPEND, 0o644)
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+		_, err = f.Write([]byte(`
+[credential]
+    helper = ` + filepath.Join(tmpdir, "fakehelper") + `
+`))
+		if !assert.NoError(t, err) {
+			t.FailNow()
+		}
+		f.Close()
+
+		_cred, err := GetGitHTTPCredential(ctx, repo, "https", "example.org")
+
+		cred, ok := _cred.(*gitHttp.BasicAuth)
+		if !assert.True(t, ok) {
+			t.FailNow()
+		}
+
+		assert.Equal(t, "user", cred.Username)      //gitleaks:allow
+		assert.Equal(t, "topsecret", cred.Password) //gitleaks:allow
+	})
 }
