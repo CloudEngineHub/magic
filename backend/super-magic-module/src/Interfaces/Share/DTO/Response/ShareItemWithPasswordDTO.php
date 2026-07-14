9a0abd69eace426d5872e8f80e2bdeea22c461ba
huanghuang <13652790703@163.com>
2026-07-14 16:43:48 +0800

    Revert "feat(share): add timestamp and view count to share item response"
    This reverts commit 77c450621f2f9cfd14e786fcfa6663d905870ddb.

--- backend/super-magic-module/src/Interfaces/Share/DTO/Response/ShareItemWithPasswordDTO.php
+++ backend/super-magic-module/src/Interfaces/Share/DTO/Response/ShareItemWithPasswordDTO.php
@@ -58,9 +58,6 @@ class ShareItemWithPasswordDTO extends ShareItemDTO
         $dto->expireAt = $baseDto->expireAt;
         $dto->expireDays = $baseDto->expireDays;
         $dto->mainFileName = $baseDto->mainFileName;
-        $dto->createdAt = $baseDto->createdAt;
-        $dto->updatedAt = $baseDto->updatedAt;
-        $dto->viewCount = $baseDto->viewCount;
 
         return $dto;
     }
