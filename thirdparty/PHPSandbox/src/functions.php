ef8c99d0f1f37493d8b2a5a4617813d380a5db20
DTYQ developers <dev@dtyq.com>
2026-07-15 10:08:49 +0800

    chore(thirdparty/PHPSandbox): Adopt code changes for upstream
    Co-authored-by: free2one <free2someone@outlook.com>
    Co-authored-by: Floken <502788062@qq.com>
    Co-authored-by: 她和她的猫 <hxhsoft@foxmail.com>
    Co-authored-by: coding huang <codinghuang@qq.com>

--- thirdparty/PHPSandbox/src/functions.php
+++ thirdparty/PHPSandbox/src/functions.php
@@ -1,66 +1,73 @@
 <?php
-    /** PHPSandbox utility functions
-     * @package PHPSandbox
-     */
-    namespace PHPSandbox;
 
-    /** Wrap output value in SandboxString
-     *
-     * @param   mixed                   $value      Value to wrap
-     * @param   PHPSandbox              $sandbox    Sandbox instance of calling code
-     *
-     * @return  mixed|SandboxedString   Returns the wrapped value
-     */
-    function wrap($value, PHPSandbox $sandbox){
-        if(!($value instanceof SandboxedString) && is_object($value) && method_exists($value, '__toString')){
-            $strval = $value->__toString();
-            return is_callable($strval) ? new SandboxedString($strval, $sandbox) : $value;
-        } else if(is_array($value) && count($value)){
-            //save current array pointer
-            $current_key = key($value);
-            foreach($value as $key => &$_value) {
-                $value[$key] = wrap($_value, $sandbox);
-            }
-            //rewind array pointer
-            reset($value);
-            //advance array to previous array key
-            while(key($value) !== $current_key){
-                next($value);
-            }
-            return $value;
-        } else if(is_string($value) && is_callable($value)){
-            return new SandboxedString($value, $sandbox);
+declare(strict_types=1);
+/**
+ * Copyright (c) The Magic , Distributed under the software license
+ */
+
+namespace PHPSandbox;
+
+use PHPSandbox\Runtime\RuntimeProxy;
+
+/** Wrap output value in SandboxString.
+ *
+ * @param mixed $value Value to wrap
+ *
+ * @return mixed|SandboxedString Returns the wrapped value
+ */
+function wrap($value, RuntimeProxy $runtimeProxy)
+{
+    if (! ($value instanceof SandboxedString) && is_object($value) && method_exists($value, '__toString')) {
+        $strval = $value->__toString();
+        return is_callable($strval) ? new SandboxedString($strval, $runtimeProxy) : $value;
+    }
+    if (is_array($value) && count($value)) {
+        // save current array pointer
+        $current_key = key($value);
+        foreach ($value as $key => &$_value) {
+            $value[$key] = wrap($_value, $runtimeProxy);
+        }
+        // rewind array pointer
+        reset($value);
+        // advance array to previous array key
+        while (key($value) !== $current_key) {
+            next($value);
         }
         return $value;
     }
-
-    /** Wrap output value in SandboxString by reference
-     *
-     * @param   mixed                   $value      Value to wrap
-     * @param   PHPSandbox              $sandbox    Sandbox instance of calling code
-     *
-     * @return  mixed|SandboxedString   Returns the wrapped value
-     */
-    function &wrapByRef(&$value, PHPSandbox $sandbox){
-        if(!($value instanceof SandboxedString) && is_object($value) && method_exists($value, '__toString')){
-            $strVal = $value->__toString();
-	        if (is_callable($strVal)) {
-		        $value = new SandboxedString($strVal, $sandbox);
-	        }
-        } else if(is_array($value) && count($value)){
-            //save current array pointer
-            $current_key = key($value);
-            foreach($value as $key => &$_value) {
-                $value[$key] = wrap($_value, $sandbox);
-            }
-            //rewind array pointer
-            reset($value);
-            //advance array to saved array pointer
-            while(key($value) !== $current_key){
-                next($value);
-            }
-        } else if(is_string($value) && is_callable($value)){
-            $value = new SandboxedString($value, $sandbox);
-        }
-        return $value;
+    if (is_string($value) && is_callable($value)) {
+        return new SandboxedString($value, $runtimeProxy);
     }
+    return $value;
+}
+
+/** Wrap output value in SandboxString by reference.
+ *
+ * @param mixed $value Value to wrap
+ *
+ * @return mixed|SandboxedString Returns the wrapped value
+ */
+function &wrapByRef(&$value, RuntimeProxy $runtimeProxy)
+{
+    if (! ($value instanceof SandboxedString) && is_object($value) && method_exists($value, '__toString')) {
+        $strVal = $value->__toString();
+        if (is_callable($strVal)) {
+            $value = new SandboxedString($strVal, $runtimeProxy);
+        }
+    } elseif (is_array($value) && count($value)) {
+        // save current array pointer
+        $current_key = key($value);
+        foreach ($value as $key => &$_value) {
+            $value[$key] = wrap($_value, $runtimeProxy);
+        }
+        // rewind array pointer
+        reset($value);
+        // advance array to saved array pointer
+        while (key($value) !== $current_key) {
+            next($value);
+        }
+    } elseif (is_string($value) && is_callable($value)) {
+        $value = new SandboxedString($value, $runtimeProxy);
+    }
+    return $value;
+}
--- thirdparty/PHPSandbox/tests/samples/multiple-includes/vendor/autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/composer/autoload_real.php';
-
-return ComposerAutoloaderInit65f31526175b9fc9733d82d2bb4caba0::test();
