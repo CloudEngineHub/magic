0daab0c25cc749be377cd439c7eae6fb55eb2fac
shensm <wy61714@gmail.com>
2026-08-06 15:46:10 +0800

    refactor(http): remove volcengine tos client classmap and add guzzle header compatibility aspect

--- backend/magic-service/config/autoload/aspects.php
+++ backend/magic-service/config/autoload/aspects.php
@@ -4,5 +4,8 @@ declare(strict_types=1);
 /**
  * Copyright (c) The Magic , Distributed under the software license
  */
+use App\Infrastructure\Util\Http\Aspect\GuzzleHeaderCompatibilityAspect;
+
 return [
+    GuzzleHeaderCompatibilityAspect::class => 10,
 ];
